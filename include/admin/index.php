46c0852214ed822f32c19fc2b903ed7dc84a592c
shimotmk <shimotmk1104@gmail.com>
2022-04-16 20:48:15 +0900

    update option method

--- include/admin/index.php
+++ include/admin/index.php
@@ -33,25 +33,27 @@ add_action(
 	function () {
 		$properties_editor_settings = array();
 		$default_editor_settings    = array();
-		$default_option_settings    = array(
-			'enqueue' => array(
-				'type'    => 'string',
-				'default' => 'head',
-			),
-			'editor'  => array(
-				'type'    => 'string',
-				'default' => 'CodeMirror',
-			),
-			// 'activationBlock' => array(
-			// 'type'    => 'array'
-			// ),
-		);
+		$default_option_settings    = abc_get_default_option();
 
 		foreach ( $default_option_settings as $key => $value ) {
 			$properties_editor_settings[ $key ] = array(
 				'type' => $value['type'],
 			);
-			$default_editor_settings[ $key ]    = $value['default'];
+
+			if ( 'object' === $value['type'] ) {
+				$default_editor_settings[ $key ]['properties'] = array();
+
+				$default_editor_settings[ $key ] = array();
+
+				foreach ( $value['items'] as $sub_key => $sub_value ) {
+					$properties_editor_options[ $key ]['properties'][ $sub_key ] = array(
+						'type' => $sub_value['type'],
+					);
+					$default_editor_settings[ $key ][ $sub_key ]                 = $sub_value['default'];
+				}
+			} else {
+				$default_editor_settings[ $key ] = $value['default'];
+			}
 		}
 		register_setting(
 			'advanced_block_css_setting',
@@ -67,20 +69,43 @@ add_action(
 				'default'      => $default_editor_settings,
 			)
 		);
-	}
+	},
+	100 // block_listの取得のために遅らせる.
 );
 
 /**
  * Get all Block.
  */
 function abc_get_all_block() {
-	$block_list     = WP_Block_Type_Registry::get_instance()->get_all_registered();
+	$block_list = WP_Block_Type_Registry::get_instance()->get_all_registered();
+	// テスト用サンプルデータ.
+	$block_list     = array(
+		'core/archives'  => (object) array(
+			'name'     => 'core/archives',
+			'title'    => 'アーカイブ',
+			'category' => 'widgets',
+			'supports' => array(
+				'customClassName' => false,
+			),
+		),
+		'core/paragraph' => (object) array(
+			'name'     => 'core/paragraph',
+			'title'    => '段落',
+			'category' => 'text',
+		),
+	);
 	$abc_block_list = array();
 	foreach ( $block_list as $key => $block ) {
+		if ( ! empty( $block->supports['customClassName'] ) && null !== $block->supports['customClassName'] ) {
+			$custom_class_name = false;
+		} else {
+			$custom_class_name = true;
+		}
 		$block_info             = array(
-			'name'     => $block->name,
-			'title'    => $block->title,
-			'category' => $block->category,
+			'name'            => $block->name,
+			'title'           => $block->title,
+			'category'        => $block->category,
+			'customClassName' => $custom_class_name,
 		);
 		$abc_block_list[ $key ] = $block_info;
 	}
@@ -116,7 +141,7 @@ add_action(
 			true
 		);
 		wp_enqueue_script( 'abc-admin-script' );
-		$abc_option = get_option( 'advanced_block_css_options' );
+		$abc_option = abc_get_option();
 		wp_localize_script( 'abc-admin-script', 'advancedBlockCssOptions', $abc_option );
 		$abc_block_list = abc_get_all_block();
 		wp_localize_script( 'abc-admin-script', 'abcBlocksList', $abc_block_list );
@@ -132,3 +157,52 @@ add_action(
 	}
 );
 
+/**
+ * Get Default Option.
+ */
+function abc_get_default_option() {
+	$block_list = abc_get_all_block();
+	foreach ( $block_list as $block ) {
+		if ( false === $block['customClassName'] ) {
+			$default = false;
+		} else {
+			$default = true;
+		}
+		if ( ! preg_match( '/^core/', $block['name'] ) ) {
+			$default = false;
+		}
+		$block_items                        = array(
+			'type'    => 'boolean',
+			'default' => $default,
+		);
+		$abc_block_object[ $block['name'] ] = $block_items;
+	}
+
+	$default_option_settings = array(
+		'enqueue' => array(
+			'type'    => 'string',
+			'default' => 'head',
+		),
+		'editor'  => array(
+			'type'    => 'string',
+			'default' => 'CodeMirror',
+		),
+		// 'activationBlock' => array(
+		// 'type'  => 'object',
+		// 'items' => $abc_block_object,
+		// ),
+	);
+	return $default_option_settings;
+}
+add_action( 'init', 'abc_get_default_option', 999 );
+
+/**
+ * Get Option.
+ */
+function abc_get_option() {
+	$default = abc_get_default_option();
+	$options = get_option( 'advanced_block_css_options' );
+	// abcBlocksListは後から追加されることがあるので、option値に保存されてない時にデフォルトとマージする.
+	$options = wp_parse_args( $options, $default );
+	return $options;
+}
