d390f83bf0e6113d6bd006c308d3e06f0153876b
shimotmk <shimotmk1104@gmail.com>
2022-04-20 20:21:00 +0900

    change directory

---
+++ includes/admin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Require File
+ *
+ * @package advanced-block-css
+ */
+
+/**
+ * Require
+ */
+require_once ADVANCED_BLOCK_CSS_DIR_PATH . 'includes/wp-plugins-list-table.php';
+require_once ADVANCED_BLOCK_CSS_DIR_PATH . 'includes/admin/index.php';
