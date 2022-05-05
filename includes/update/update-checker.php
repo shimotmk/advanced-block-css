413835d882c1b55cc4ae791f42285dbdad2bafd3
shimotmk <shimotmk1104@gmail.com>
2022-05-05 19:18:31 +0900

    path

--- includes/update/update-checker.php
+++ includes/update/update-checker.php
@@ -12,7 +12,7 @@ require ADVANCED_BLOCK_CSS_DIR_PATH . 'includes/update/plugin-update-checker/plu
 
 $abc_update_checker = Puc_v4_Factory::buildUpdateChecker(
 	'https://github.com/shimotmk/advanced-block-css/',
-	__FILE__,
+	ADVANCED_BLOCK_CSS_DIR_PATH,
 	'advanced-block-css'
 );
 
